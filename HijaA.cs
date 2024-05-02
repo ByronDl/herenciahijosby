ded45e96fb144daa13f954c62b7ddcc52520fe52
ByronDl <159460555+ByronDl@users.noreply.github.com>
2024-05-02 08:34:38 -0600

    Agregar archivos de proyecto.

---
+++ HijaA.cs
@@ -0,0 +1,16 @@
+﻿namespace EjemploHerencia
+{
+	public class HijaA : Padre //Simplemente con esto es como si haya copiado los datos de otra clase y se los haya asignado a esta 
+	{
+		public string carne { get; set; }
+
+		public int numNotas { get; set; }
+
+		public HijaA()
+		{
+			carne = string.Empty; //Otra manera de poner las comillas o el valor nulo
+		}
+	}
+}
+
+//hola mundo internet 
---
+++ Tio.cs
@@ -0,0 +1,9 @@
+﻿namespace EjemploHerencia
+{
+	public class Tio : Padre
+	{
+		public string titulo { get; set; }
+
+		public int noColegiado { get; set; }
+	}
+}
